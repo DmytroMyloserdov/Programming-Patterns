b7f0d4d0750b693c7ed7a3150e1fbc2af8a47574
DmytroMyloserdov <dagger.dager@gmail.com>
2018-08-03 15:48:45 +0300

    Added public Factory`s caller

---
+++ Patterns/GenerativePatterns/Factory/FactoryCaller.cs
@@ -0,0 +1,18 @@
+﻿namespace Patterns.GenerativePatterns.Factory
+{
+    public class FactoryCaller
+    {
+        public static void Call()
+        {
+            CompanyCreator companyCreator = new WeaponCompanyCreator();
+            var weaponCompany = companyCreator.Create("Military Ware", 10000);
+            weaponCompany.CreateProduct();
+            weaponCompany.SellProduct();
+
+            companyCreator = new ToyCompanyCreator();
+            var toyCompany = companyCreator.Create("Child Fun", 100);
+            toyCompany.CreateProduct();
+            toyCompany.SellProduct();
+        }
+    }
+}
